7438cb4c2ecc305a88e20788bb0e43dbb61bbd13
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-11-24 14:44:53 -0600

    re-implement pending flow spec

--- spec/features/idv/flow_spec.rb
+++ spec/features/idv/flow_spec.rb
@@ -55,8 +55,6 @@ feature 'IdV session', idv_job: true do
     end
 
     scenario 'vendor agent throws exception' do
-      pending 'this will raise now. need to rescue that error somehow'
-
       first_name_to_trigger_exception = 'Fail'
 
       sign_in_and_2fa_user
@@ -65,9 +63,22 @@ feature 'IdV session', idv_job: true do
 
       fill_out_idv_form_ok
       fill_in 'profile_first_name', with: first_name_to_trigger_exception
+
+      expect(Idv::ProfileJob).to receive(:perform_now).and_wrap_original do |perform, *args|
+        exception_raised = false
+        begin
+          perform.call(*args)
+        rescue RuntimeError => err
+          expect(err.message).to eq('Failed to contact proofing vendor')
+          exception_raised = true
+        ensure
+          expect(exception_raised).to eq(true)
+        end
+      end
+
       click_idv_continue
 
-      expect(current_path).to eq verify_session_result_path
+      expect(current_path).to eq(verify_session_result_path)
       expect(page).to have_css('.modal-warning', text: t('idv.modal.sessions.heading'))
     end
 
